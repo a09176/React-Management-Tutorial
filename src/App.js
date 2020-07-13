c538cce2d4c2dc42324d5f353a5fe613594c436c
김아영 <a09176@gmail.com>
2020-07-13 17:18:16 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Customer from './components/Customer';
+
+const customers = [{
+'id' : 1,
+'image' : 'https://placeimg.com/64/64/1', 
+'name' : '김하나',
+'birthday': '010206',
+'gender' : '여',
+'job' : '개발자'
+},
+{
+  'id' : 2,
+  'image' : 'https://placeimg.com/64/64/2', 
+  'name' : '김둘',
+  'birthday': '010204',
+  'gender' : '남',
+  'job' : '직장인'
+  },
+  {
+    'id' : 3,
+    'image' : 'https://placeimg.com/64/64/3', 
+    'name' : '김셋',
+    'birthday': '010203',
+    'gender' : '여',
+    'job' : '선생님'
+  } 
+]
 class App extends Component {
   render() {
   return (
-    <div className="gray-background">
-        <img src={logo} alt="logo"/>
-        <h2>Let's develop management system!</h2>
-    </div>
+    <div>
+      {customers.map(c => {return (<Customer key = {c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />);})}
+   </div>
   );
   }
 }
