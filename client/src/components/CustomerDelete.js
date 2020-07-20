b413f16cc8094f1ce2c1ef575e947b75155a712a
김아영 <a09176@gmail.com>
2020-07-20 16:41:13 +0900

    Add Modal Module

--- client/src/components/CustomerDelete.js
+++ client/src/components/CustomerDelete.js
@@ -1,6 +1,19 @@
 import React from 'react';
-
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 class CustomerDelete extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            open : false
+        }
+    }
+
 
     deleteCustomer(id){
         const url = "/api/customers/"+id;
@@ -9,9 +22,39 @@ class CustomerDelete extends React.Component{
         });
         this.props.stateRefresh();
     }
+
+    handleClickOpen = () => {
+        this.setState({
+            open : true
+        })
+    }
+
+    handleClickClose = () => {
+        this.setState({
+            open : false 
+        });
+    }
+
     render(){
         return(
-            <button onClick={(e)=>{this.deleteCustomer(this.props.id)}}>삭제</button>
+            <div>
+                <Button variant="contained" color="secondary" onClick={this.handleClickOpen} >삭제</Button>
+                <Dialog open={this.state.open} onClose={this.handleClickClose}>
+                    <DialogTitle onClose={this.handleClickClose}>
+                        삭제경고!
+                    </DialogTitle>
+                    <DialogContent>
+                        <Typography gutterBottom>
+                             해당 고객을 삭제하시겠습니까?
+                        </Typography>
+                    </DialogContent>
+                    <DialogActions>
+                    <Button variant="contained" color="primary" onClick={(e)=>{this.deleteCustomer(this.props.id)}}>삭제</Button>
+                    <Button variant="outlined" color="primary" onClick={this.handleClickClose}>닫기</Button>
+                    </DialogActions>
+                </Dialog>
+            </div>
+            
         );
     }
 
